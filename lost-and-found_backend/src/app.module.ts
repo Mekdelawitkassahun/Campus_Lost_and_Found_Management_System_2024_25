cd37cd15f856066266b7029b344b6e01e0a77c94
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-01-09 22:06:54 +0300

    app module

--- lost-and-found_backend/src/app.module.ts
+++ lost-and-found_backend/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { ClaimModule } from './claim/claim.module';
+import { LostItemsModule } from './lost-items/lost-items.module';
+import { LostItemsController } from './lost-items/lost-items.controller';
+import { AuthGuard } from './auth/Guard/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    ClaimModule,
+    LostItemsModule,
+  ],
+  controllers: [AppController, LostItemsController],
   providers: [AppService],
 })
 export class AppModule {}
